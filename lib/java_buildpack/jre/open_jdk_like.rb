de2c4e14f4afa8cba441fec6fbda8b6f254f66f3
Christopher Frost <cfrost@pivotal.io>
2015-01-06 12:17:45 +0000

    Auto detect Java major version
    The commit adds the ability to turn on Java version detection. This
    will scan the application to detect the major version of Java that
    should be used for the application and then refer to the config file
    for the exact version to use.
    
    [#71382852]

--- lib/java_buildpack/jre/open_jdk_like.rb
+++ lib/java_buildpack/jre/open_jdk_like.rb
@@ -15,15 +15,18 @@
 # limitations under the License.
 
 require 'fileutils'
+require 'find'
 require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/jre'
 require 'java_buildpack/jre/memory/openjdk_memory_heuristic_factory'
+require 'zip'
 
 module JavaBuildpack
   module Jre
 
     # Encapsulates the detect, compile, and release functionality for selecting an OpenJDK-like JRE.
     class OpenJDKLike < JavaBuildpack::Component::VersionedDependencyComponent
+      include Find
 
       # Creates an instance
       #
@@ -33,15 +36,20 @@ module JavaBuildpack
         @component_name = self.class.to_s.space_case
         @configuration  = context[:configuration]
         @droplet        = context[:droplet]
+        @logger         = JavaBuildpack::Logging::LoggerFactory.instance.get_logger OpenJDKLike
 
         @droplet.java_home.root = @droplet.sandbox
       end
 
       # (see JavaBuildpack::Component::BaseComponent#detect)
       def detect
-        @version, @uri             = JavaBuildpack::Repository::ConfiguredItem.find_item(@component_name,
-                                                                                         @configuration)
+        version       = detect_compiled?(@configuration[KEY_VERSION]) ? compiled_version(@application.root) : VERSION_8
+        configuration = { KEY_REPOSITORY_ROOT => @configuration[KEY_REPOSITORY_ROOT],
+                          KEY_VERSION         => @configuration[KEY_VERSION][version] }
+
+        @version, @uri             = JavaBuildpack::Repository::ConfiguredItem.find_item(@component_name, configuration)
         @droplet.java_home.version = @version
+
         super
       end
 
@@ -53,28 +61,94 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
+        version = detect_compiled?(@configuration[KEY_VERSION]) ? compiled_version(@application.root) : VERSION_8
+
         @droplet.java_opts
           .add_system_property('java.io.tmpdir', '$TMPDIR')
           .add_option('-XX:OnOutOfMemoryError', killjava)
-          .concat memory
+          .concat memory(version)
       end
 
       private
+
+      CAFEBABE = 'cafebabe'.freeze
+
+      KEY_DETECT_COMPILED_VERSION = 'detect_compiled'.freeze
 
       KEY_MEMORY_HEURISTICS = 'memory_heuristics'.freeze
 
       KEY_MEMORY_SIZES = 'memory_sizes'.freeze
 
-      private_constant :KEY_MEMORY_HEURISTICS, :KEY_MEMORY_SIZES
+      KEY_REPOSITORY_ROOT = 'repository_root'.freeze
+
+      KEY_VERSION = 'version'.freeze
+
+      VERSION_6 = 6.freeze
+
+      VERSION_7 = 7.freeze
+
+      VERSION_8 = 8.freeze
+
+      private_constant :CAFEBABE, :KEY_DETECT_COMPILED_VERSION, :KEY_MEMORY_HEURISTICS, :KEY_MEMORY_SIZES,
+                       :KEY_REPOSITORY_ROOT, :KEY_VERSION, :VERSION_6, :VERSION_7, :VERSION_8
+
+      def class?(path)
+        File.extname(path) == '.class'
+      end
+
+      def class_file_format(path)
+        bits = File.open(path).read.unpack('H*')[0]
+        @logger.debug { "Scanning #{path}:  #{bits[0, 8]}/#{bits[14, 2]}" }
+
+        Integer(bits[14, 2]) if magic_number?(bits)
+      end
+
+      def compiled_version(root)
+        @logger.debug { 'Detecting compiled version' }
+
+        versions = find(root.to_path).map do |child|
+          next if File.directory?(child) || !class?(child)
+          class_file_format child
+        end
+
+        version versions.max_by(&:to_i)
+      end
+
+      def detect_compiled?(configuration)
+        configuration[KEY_DETECT_COMPILED_VERSION] == 'enabled'
+      end
 
       def killjava
         @droplet.sandbox + 'bin/killjava.sh'
       end
 
-      def memory
-        sizes      = @configuration[KEY_MEMORY_SIZES] || {}
-        heuristics = @configuration[KEY_MEMORY_HEURISTICS] || {}
+      def magic_number?(bits)
+        bits[0, 8] == CAFEBABE
+      end
+
+      def memory(version)
+        sizes      = @configuration[KEY_MEMORY_SIZES] ? @configuration[KEY_MEMORY_SIZES].clone : {}
+        heuristics = @configuration[KEY_MEMORY_HEURISTICS] ? @configuration[KEY_MEMORY_HEURISTICS].clone : {}
+
+        if version < VERSION_8
+          heuristics.delete 'metaspace'
+          sizes.delete 'metaspace'
+        else
+          heuristics.delete 'permgen'
+          sizes.delete 'permgen'
+        end
+
         OpenJDKMemoryHeuristicFactory.create_memory_heuristic(sizes, heuristics, @version).resolve
+      end
+
+      def version(format)
+        if format == 32
+          VERSION_6
+        elsif format == 33
+          VERSION_7
+        else
+          VERSION_8
+        end
       end
 
     end
