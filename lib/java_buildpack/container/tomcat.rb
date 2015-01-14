f2f2f428681978795ec5d75e211f489a5db79f12
Christopher Frost <cfrost@pivotal.io>
2015-01-14 16:27:26 +0000

    Add support for Gem Fire session replication
    This commit adds support for automatic Tomcat session replication using
    Gem Fire by binding to a service that exposes the required service
    tag 'session_replication' and service credentials 'locators',
    'username' and 'password'.
    
    [#81541998]

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -22,6 +22,7 @@ require 'java_buildpack/container/tomcat/tomcat_lifecycle_support'
 require 'java_buildpack/container/tomcat/tomcat_logging_support'
 require 'java_buildpack/container/tomcat/tomcat_access_logging_support'
 require 'java_buildpack/container/tomcat/tomcat_redis_store'
+require 'java_buildpack/container/tomcat/tomcat_gemfire_store'
 
 module JavaBuildpack
   module Container
@@ -51,6 +52,7 @@ module JavaBuildpack
           TomcatLoggingSupport.new(sub_configuration_context(context, 'logging_support')),
           TomcatAccessLoggingSupport.new(sub_configuration_context(context, 'access_logging_support')),
           TomcatRedisStore.new(sub_configuration_context(context, 'redis_store')),
+          TomcatGemfireStore.new(sub_configuration_context(context, 'gemfire_store')),
           TomcatInsightSupport.new(context)
         ]
       end
