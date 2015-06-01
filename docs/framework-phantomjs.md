0f059cbfe8caf57f575b69b07414c17221310306
Claude Devarenne <cdevarenne@Claude-Devarenne-MacBook-Pro.local>
2015-06-01 15:43:06 -0700

    initial commit

--- docs/framework-phantomjs.md
+++ docs/framework-phantomjs.md
@@ -1,30 +1,23 @@
-# Spring Insight Framework
-The Spring Insight Framework causes an application to be automatically configured to work with a bound [Spring Insight Service][]. This feature will only work with Spring Insight versions of 2.0.0.x or above.
+# PhantomJS Framework
+Add PhantomJS binary for Highcharts export server when docker is not an option or refactoring the highcharts export server to use PhantomJS as a separate service is not an option.
 
 <table>
   <tr>
     <td><strong>Detection Criterion</strong></td>
-    <td>Existence of a single bound Spring Insight service.
-      <ul>
-        <li>Existence of a Spring Insight service is defined as the <a href="http://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#VCAP-SERVICES"><code>VCAP_SERVICES</code></a> payload containing a service who's name, label or tag has <code>insight</code> as a substring.</li>
-      </ul>
+    <td>Existence of a export-servlet.xml file inside the application archive. This file should be present in every highcharts-export-web.
     </td>
   </tr>
   <tr>
     <td><strong>Tags</strong></td>
-    <td><tt>spring-insight=&lt;version&gt;</tt></td>
+    <td><tt>phantomjs=&lt;version&gt;</tt></td>
   </tr>
 </table>
 Tags are printed to standard output by the buildpack detect script
 
-## User-Provided Service
-When binding Spring Insight using a user-provided service, it must have name or tag with `insight` in it.  The credential payload must contain the following entries:
-
-| Name | Description
-| ---- | -----------
-| `dashboard_url` | The URL via which users access the Spring Insight dashboard.
-
 ## Configuration
-The Spring Insight Framework cannot be configured.
+For general information on configuring the buildpack, refer to [Configuration and Extension][].
 
-[Spring Insight Service]: http://gopivotal.com/pivotal-products/apps/pivotal-tc-server
+The framework can be configured by modifying the [`config/phantom_js.yml`][] file in the buildpack fork.  The framework uses the [`Repository` utility support][repositories] and so it supports the [version syntax][] defined there.
+
+[HighchartsExportServer]: http://www.highcharts.com/docs/export-module/setting-up-the-server
+[PhantomsJS]: http://phantomjs.org/
--- lib/java_buildpack/framework/phantom_js.rb
+++ lib/java_buildpack/framework/phantom_js.rb
@@ -14,193 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'java_buildpack/component/base_component'
-require 'java_buildpack/util/cache/internet_availability'
+require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/framework'
 require 'java_buildpack/util/dash_case'
+require 'java_buildpack/util/shell'
 require 'tmpdir'
-require 'fileutils'
-require 'uri'
 
 module JavaBuildpack
   module Framework
 
-    # Encapsulates the detect, compile, and release functionality for enabling Insight auto configuration.
-    class SpringInsight < JavaBuildpack::Component::BaseComponent
+    # Encapsulates the functionality for enabling PhantomJS for highcharts-export-web app.
+    class PhantomJS < JavaBuildpack::Component::VersionedDependencyComponent
+#      include JavaBuildpack::Util::Shell
 
       # Creates an instance
       #
-      # @param [Hash] context a collection of utilities used the component
-      def initialize(context)
-        super(context)
-        @version, @uri, @agent_id, @agent_pass = supports? ? find_insight_agent : [nil, nil, nil, nil]
-      end
-
-      # (see JavaBuildpack::Component::BaseComponent#detect)
-      def detect
-        @version ? id(@version) : nil
+      # @param [Hash] context a collection of utilities used by the component
+      def initialize(context, &version_validator)
+        super(context, &version_validator)
+        @component_name = 'Phantom JS'
       end
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
-        JavaBuildpack::Util::Cache::InternetAvailability.instance.available(
-          true, 'The Spring Insight download location is always accessible') do
-          # TODO: AGENT_DOWNLOAD_URI_SUFFIX To be removed once the full path is included in VCAP_SERVICES see #58873498
-          download(@version, @uri.chomp('/') + AGENT_DOWNLOAD_URI_SUFFIX) { |file| expand file }
-        end
+        download_tar
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-        @droplet.java_opts
-          .add_javaagent(weaver_jar)
-          .add_system_property('insight.base', insight_directory)
-          .add_system_property('insight.logs', logs_directory)
-          .add_system_property('aspectj.overweaving', true)
-          .add_system_property('org.aspectj.tracing.factory', 'default')
-          .add_system_property('insight.transport.type', 'HTTP')
-
-        add_agent_configuration
+        # execute build steps for PhantomJS binary
+        shell "sudo apt-get update"
+        shell "sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y"
+        shell "sudo apt-get install libfreetype6 libfreetype6-dev -y"
+        shell "sudo apt-get install libfontconfig1 libfontconfig1-dev -y"
+        shell "cd /tmp"
+        shell "wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2"
+        shell "sudo tar xjf $PHANTOM_JS.tar.bz2"
+        shell "sudo mv $PHANTOM_JS /usr/local/share"
+        shell "sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin"
       end
 
       protected
-
-      # The unique identifier of the component, incorporating the version of the dependency (e.g.
-      # +spring-insight=1.9.3+)
-      #
-      # @param [String] version the version of the dependency
-      # @return [String] the unique identifier of the component
-      def id(version)
-        "#{SpringInsight.to_s.dash_case}=#{version}"
+      # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
+      def supports?
+        phantomjs_configured?(@application.root + 'WEB-INF/spring')
       end
 
       private
 
-      # TODO: To be removed once the full path is included in VCAP_SERVICES see issue 58873498
-      AGENT_DOWNLOAD_URI_SUFFIX = '/services/config/agent-download'.freeze
-
-      FILTER = /insight/.freeze
-
-      private_constant :AGENT_DOWNLOAD_URI_SUFFIX, :FILTER
-
-      def add_agent_configuration
-        @droplet.java_opts
-          .add_system_property('agent.http.protocol', 'http')
-          .add_system_property('agent.http.host', URI(@uri).host)
-          .add_system_property('agent.http.port', 80)
-          .add_system_property('agent.http.context.path', 'insight')
-          .add_system_property('agent.http.username', @agent_id)
-          .add_system_property('agent.http.password', @agent_pass)
-          .add_system_property('agent.http.send.json', false)
-          .add_system_property('agent.http.use.proxy', false)
-      end
-
-      def expand(file)
-        with_timing "Expanding Spring Insight to #{@droplet.sandbox.relative_path_from(@droplet.root)}" do
-          Dir.mktmpdir do |root|
-            agent_dir = unpack_agent_installer(Pathname.new(root), file)
-            install_insight(agent_dir)
-          end
-        end
-      end
-
-      def unpack_agent_installer(root, file)
-        installer_dir = root + 'installer'
-        agent_dir     = root + 'agent'
-
-        FileUtils.mkdir_p(installer_dir)
-        FileUtils.mkdir_p(agent_dir)
-        shell "unzip -qq #{file.path} -d #{installer_dir} 2>&1"
-        shell "unzip -qq #{uber_agent_zip(installer_dir)} -d #{agent_dir} 2>&1"
-
-        agent_dir
-      end
-
-      def install_insight(agent_dir)
-        root = Pathname.glob(agent_dir + 'springsource-insight-uber-agent-*')[0]
-
-        init_container_libs root
-        init_insight_cloudfoundry_agent_plugin root
-        init_insight root
-        init_insight_agent_plugins root
-        init_weaver root
-      end
-
-      def init_container_libs(root)
-        move container_libs_directory,
-             root + 'agents/common/insight-bootstrap-generic-*.jar',
-             root + 'agents/tomcat/7/lib/insight-bootstrap-tomcat-common-*.jar',
-             root + 'agents/tomcat/7/lib/insight-agent-*.jar'
-      end
-
-      def init_insight(root)
-        move insight_directory,
-             root + 'insight/collection-plugins',
-             root + 'insight/conf'
-      end
-
-      def init_insight_agent_plugins(root)
-        move insight_directory + 'agent-plugins',
-             root + 'transport/http/insight-agent-http-*.jar',
-             root + 'cloudfoundry/insight-agent-cloudfoundry-*.jar'
-      end
-
-      def init_insight_cloudfoundry_agent_plugin(root)
-        move container_libs_directory,
-             root + 'cloudfoundry/cloudfoundry-runtime-*.jar'
-      end
-
-      def init_weaver(root)
-        move weaver_directory,
-             root + 'cloudfoundry/insight-weaver-*.jar'
-      end
-
-      def container_libs_directory
-        @droplet.root + '.spring-insight/container-libs'
-      end
-
-      def find_insight_agent
-        service     = @application.services.find_service FILTER
-        version     = service['label'].match(/(.*)-(.*)/)[2]
-        credentials = service['credentials']
-        uri         = credentials['dashboard_url']
-        id          = credentials['agent_username']
-        pass        = credentials['agent_password']
-        [version, uri, id, pass]
-      end
-
-      def insight_directory
-        @droplet.sandbox + 'insight'
-      end
-
-      def logs_directory
-        insight_directory + 'logs'
-      end
-
-      def move(destination, *globs)
-        FileUtils.mkdir_p destination
-
-        globs.each do |glob|
-          FileUtils.mv Pathname.glob(glob)[0], destination
-        end
-      end
-
-      def supports?
-        @application.services.one_service? FILTER, 'dashboard_url', 'agent_username', 'agent_password'
-      end
-
-      def uber_agent_zip(location)
-        candidates = Pathname.glob(location + 'springsource-insight-uber-agent-*.zip')
-        fail 'There was not exactly one Uber Agent zip' if candidates.size != 1
-        candidates[0]
-      end
-
-      def weaver_directory
-        @droplet.sandbox + 'weaver'
-      end
-
-      def weaver_jar
-        (weaver_directory + 'insight-weaver-*.jar').glob[0]
+      def phantomjs_configured?(root_path)
+        (root_path + 'export-servlet.xml').exist?
       end
 
     end
