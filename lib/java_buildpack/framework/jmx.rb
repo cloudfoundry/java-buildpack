6c861beacc1dd477eed1bb54c0ee6968cb1744e8
Ben Hale <bhale@pivotal.io>
2015-11-05 16:01:28 -0800

    JMX RMI Port Fix
    This change updates the JMX framework to fix a problem where the port
    number of the JMX RMI server overlapped with the JMX connector port.

--- lib/java_buildpack/framework/jmx.rb
+++ lib/java_buildpack/framework/jmx.rb
@@ -39,7 +39,7 @@ module JavaBuildpack
           .add_system_property('java.rmi.server.hostname', '127.0.0.1')
           .add_system_property('com.sun.management.jmxremote.authenticate', false)
           .add_system_property('com.sun.management.jmxremote.port', port)
-          .add_system_property('com.sun.management.jmxremote.rmi.port', port)
+          .add_system_property('com.sun.management.jmxremote.rmi.port', port + 1)
       end
 
       private
