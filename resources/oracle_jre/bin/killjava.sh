9dc18fa4c259d3937e52b653a101a3815800d669
Ben Hale <bhale@pivotal.io>
2016-03-09 07:49:04 -0800

    Further Copyright Updates
    This change updates some more of the codebase with an updated copyright 
    statement.

--- resources/oracle_jre/bin/killjava.sh
+++ resources/oracle_jre/bin/killjava.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Encoding: utf-8
 # Cloud Foundry Java Buildpack
-# Copyright (c) 2013 the original author or authors.
+# Copyright 2013-2016 the original author or authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
