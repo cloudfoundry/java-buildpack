b634c92fb55af8956dcbaf5516d0973aa896c4dd
Ben Hale <bhale@pivotal.io>
2016-08-18 13:28:51 -0700

    Polishing

--- ci/auto-merge.sh
+++ ci/auto-merge.sh
@@ -23,7 +23,7 @@ popd
 git clone downstream merged
 
 pushd merged
-  git config --local user.name $GIT_USER_NAME
+  git config --local user.name "$GIT_USER_NAME"
   git config --local user.email $GIT_USER_EMAIL
 
   git remote add upstream ../upstream
