3cd6d239a293e2b20acacf84e8b11bad7d8b2e36
Ben Hale <bhale@pivotal.io>
2017-01-06 12:41:46 -0800

    Copyright Updates

--- lib/java_buildpack/container/spring_boot_cli.rb
+++ lib/java_buildpack/container/spring_boot_cli.rb
@@ -1,9 +1,9 @@
 # Encoding: utf-8
 # Cloud Foundry Java Buildpack
-# Copyright 2013-2016 the original author or authors.
+# Copyright 2013-2017 the original author or authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this candidate except in compliance with the License.
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
