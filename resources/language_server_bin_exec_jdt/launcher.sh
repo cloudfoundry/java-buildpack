7f4471f54b093ca78095873a06115356fc3f3f90
Sosin <pavel.sosin@sap.com>
2017-04-04 10:13:49 +0300

    Set launcher env

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
-EXPORT STDIN_PORT=8991
-EXPORT STDOUT_PORT=8990
+STDIN_PORT=8991; export STDIN_PORT
+STDOUT_PORT=8990; export STDOUT_PORT
 $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/language_server_bin_exec_jdt/jdt_ws_root
