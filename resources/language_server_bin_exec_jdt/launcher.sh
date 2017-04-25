6a5b65335a160c91c58ab25ff2d060737d1a0f81
Sosin <pavel.sosin@sap.com>
2017-04-25 11:52:39 +0300

    trap INT TERM

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -2,5 +2,5 @@
 #declare -i STDIN_PORT=8991; export STDIN_PORT
 #declare -i STDOUT_PORT=8990; export STDOUT_PORT
 #
-trap "pkill -P $$" SIGINT SIGTERM
+trap "pkill -P $$" INT TERM
 $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/language_server_bin_exec_jdt/jdt_ws_root <&0 2>&1
