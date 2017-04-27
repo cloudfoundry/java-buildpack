3e3ff8a66e5a87cb8e045f3196c0f0f2a1d77147
Sosin <pavel.sosin@sap.com>
2017-04-27 15:40:42 +0300

    Use function and TERM

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -2,5 +2,8 @@
 #declare -i STDIN_PORT=8991; export STDIN_PORT
 #declare -i STDOUT_PORT=8990; export STDOUT_PORT
 #
-# trap "pkill -P $$" SIGINT SIGTERM
+function kill_lsp {
+    pkill -P $$
+}
+trap kill_lsp TERM
 $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root <&0 2>&1
