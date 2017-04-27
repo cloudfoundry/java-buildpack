2f713a34b14c3477a1d34d9a9514ed56a7b66b81
Sosin <pavel.sosin@sap.com>
2017-04-27 16:25:45 +0300

    use 5980

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -2,8 +2,8 @@
 #declare -i STDIN_PORT=8991; export STDIN_PORT
 #declare -i STDOUT_PORT=8990; export STDOUT_PORT
 #
-function kill_lsp {
-    pkill -P $$
+kill_lsp () {
+    pkill -P $BASHPID
 }
 trap kill_lsp TERM
-$JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root <&0 2>&1
+$JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_rTER
