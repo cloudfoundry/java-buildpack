d1a9e6e03a5541b95c5713b24511d2bda8ecac59
Ben Hale <bhale@pivotal.io>
2017-05-05 14:35:17 -0700

    Versions Output Polishing
    This change adds the unknown ID to the unknown name and sorts names ignoring
    case.

--- rakelib/versions_task.rb
+++ rakelib/versions_task.rb
@@ -178,7 +178,7 @@ module Package
 
           dependency_versions << {
             'id'      => id,
-            'name'    => NAME_MAPPINGS[id] || 'UNKNOWN',
+            'name'    => NAME_MAPPINGS[id] || "UNKNOWN (#{id})",
             'uri'     => uri,
             'version' => version
           }
@@ -212,7 +212,7 @@ module Package
         v    = versions
 
         rows = v['dependencies']
-               .sort_by { |dependency| dependency['name'] }
+               .sort_by { |dependency| dependency['name'].downcase }
                .map { |dependency| [dependency['name'], dependency['version']] }
 
         puts Terminal::Table.new title: "Java Buildpack #{v['buildpack']}", rows: rows
