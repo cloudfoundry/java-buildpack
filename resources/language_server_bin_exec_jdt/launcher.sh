6d63021b8357df15ba02a5cdac028d80b1680440
Sosin <pavel.sosin@sap.com>
2017-08-06 16:09:15 +0300

    multiproject with separate JDT WS

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ -z $2] then
-    if [ -z $3] then
-        moduleWs = "$2/$3"
-    else 
-        moduleWs = "$2"
-    fi
-    if [ -e -d "$HOME/jdt_ws_root/$moduleWs"] then
-        echo 
-    else
-        mkdir $HOME/jdt_ws_root/$moduleWs
-    fi
+if [ -z "$2"]; then
+if [ -z "$3"]; then
+    moduleWs="$2/$3"
+else 
+    moduleWs="$2"
+fi
+
+if [  "$HOME/jdt_ws_root/$moduleWs"  ] && [ -z moduleWs ]; then
+    echo "Already exists $moduleWs"
+else
+    echo "Create $HOME/jdt_ws_root/$moduleWs"
+    mkdir $HOME/jdt_ws_root/$moduleWs
+fi
     exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
 else
     exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root
