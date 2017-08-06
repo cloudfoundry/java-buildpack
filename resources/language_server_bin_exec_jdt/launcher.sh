e67ba25434dad0d4aef79735c864739831591f1b
Sosin <pavel.sosin@sap.com>
2017-08-06 15:15:08 +0300

    multiproject with separate JDT WS

--- resources/language_server_bin_exec_jdt/launcher.sh
+++ resources/language_server_bin_exec_jdt/launcher.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-#declare -i STDIN_PORT=8991; export STDIN_PORT
-#declare -i STDOUT_PORT=8990; export STDOUT_PORT
-#
-#kill_lsp () {
-#    pkill -P $BASHPID
-#}
-#trap kill_lsp TERM
-exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root
+
+if [ -z $2] 
+    if [ -z $3]
+        moduleWs = "$2/$3""
+    else 
+        moduleWs = "$2"
+    fi
+    if [ -e -d "$HOME/jdt_ws_root/$moduleWs"]
+        echo 
+    else
+        mkdir $HOME/jdt_ws_root/$moduleWs
+    fi
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root/$moduleWs
+else
+    exec $JAVA_HOME/bin/java -Declipse.application=org.eclipse.jdt.ls.core.id1 -Dosgi.bundles.defaultStartLevel=4 -Declipse.product=org.eclipse.jdt.ls.core.product -Dlog.protocol=true -Dlog.level=ALL -noverify -Xmx1G -XX:+UseG1GC -XX:+UseStringDeduplication -jar ./plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -configuration ./config_linux -data $HOME/jdt_ws_root
+fi
