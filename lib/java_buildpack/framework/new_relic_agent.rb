7f36d88bf4b86803b5e2352fee5a51929f086bd0
Sven Walter <Sven.Walter4@de.bosch.com>
2017-10-16 22:56:44 +0200

    Fix issue with missing character to start comment

--- lib/java_buildpack/framework/new_relic_agent.rb
+++ lib/java_buildpack/framework/new_relic_agent.rb
@@ -1,4 +1,4 @@
-Cloud Foundry Java Buildpack
+# Cloud Foundry Java Buildpack
 # Copyright 2013-2017 the original author or authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
