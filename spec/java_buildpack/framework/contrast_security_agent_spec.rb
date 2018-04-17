69b70e67e317f67575bc2c1077982cc712b30c85
Donnie Propst <donniepropst@gmail.com>
2018-04-17 11:36:53 -0400

    Test that the default contrast.override.appname isn't present

--- spec/java_buildpack/framework/contrast_security_agent_spec.rb
+++ spec/java_buildpack/framework/contrast_security_agent_spec.rb
@@ -90,6 +90,7 @@ describe JavaBuildpack::Framework::ContrastSecurityAgent do
       java_opts.add_system_property('contrast.override.appname', 'NAME_ALREADY_OVERRIDDEN')
       component.release
       expect(java_opts).to include('-Dcontrast.override.appname=NAME_ALREADY_OVERRIDDEN')
+      expect(java_opts).to_not include('-Dcontrast.override.appname=test-application-name')
     end
 
   end
