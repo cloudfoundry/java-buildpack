809536461574d7f2280ef66814ce8801ef569af0
Ben Hale <bhale@pivotal.io>
2018-10-08 09:09:04 -0700

    Azure Application Insights Agent
    This change adds support for the Azure Application Insights Agent.
    
    [resolves #620]

---
+++ lib/java_buildpack/framework/azure_application_insights_agent.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2018 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'fileutils'
+require 'java_buildpack/component/versioned_dependency_component'
+require 'java_buildpack/framework'
+
+module JavaBuildpack
+  module Framework
+
+    # Encapsulates the functionality for enabling Azure Application Insights support.
+    class AzureApplicationInsightsAgent < JavaBuildpack::Component::VersionedDependencyComponent
+
+      # (see JavaBuildpack::Component::BaseComponent#compile)
+      def compile
+        download_jar
+        @droplet.copy_resources
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#release)
+      def release
+        credentials = @application.services.find_service(FILTER, INSTRUMENTATION_KEY)['credentials']
+
+        @droplet
+          .java_opts.add_javaagent(@droplet.sandbox + jar_name)
+          .add_system_property('APPLICATION_INSIGHTS_IKEY', credentials[INSTRUMENTATION_KEY])
+      end
+
+      protected
+
+      # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
+      def supports?
+        @application.services.one_service? FILTER, INSTRUMENTATION_KEY
+      end
+
+      FILTER = /azure-application-insights/
+
+      INSTRUMENTATION_KEY = 'instrumentation_key'
+
+      private_constant :FILTER, :INSTRUMENTATION_KEY
+
+    end
+
+  end
+end
---
+++ spec/java_buildpack/framework/azure_application_insights_agent_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# Cloud Foundry Java Buildpack
+# Copyright 2013-2018 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+require 'component_helper'
+require 'java_buildpack/framework/azure_application_insights_agent'
+
+describe JavaBuildpack::Framework::AzureApplicationInsightsAgent do
+  include_context 'with component help'
+
+  let(:configuration) do
+    { 'default_application_name' => nil }
+  end
+
+  it 'does not detect without azure-application-insights service' do
+    expect(component.detect).to be_nil
+  end
+
+  context do
+
+    before do
+      allow(services).to receive(:one_service?).with(/azure-application-insights/, 'instrumentation_key')
+                                               .and_return(true)
+    end
+
+    it 'detects with azure-application-insights service' do
+      expect(component.detect).to eq("azure-application-insights-agent=#{version}")
+    end
+
+    it 'downloads Azure Application Insights agent JAR',
+       cache_fixture: 'stub-azure-application-insights-agent.jar' do
+
+      component.compile
+
+      expect(sandbox + "azure_application_insights_agent-#{version}.jar").to exist
+    end
+
+    it 'copies resources',
+       cache_fixture: 'stub-azure-application-insights-agent.jar' do
+
+      component.compile
+
+      expect(sandbox + 'AI-Agent.xml').to exist
+    end
+
+    it 'updates JAVA_OPTS' do
+      allow(services).to receive(:find_service)
+        .and_return('credentials' => { 'instrumentation_key' => 'test-instrumentation-key' })
+
+      component.release
+
+      expect(java_opts).to include('-javaagent:$PWD/.java-buildpack/azure_application_insights_agent/' \
+                                   "azure_application_insights_agent-#{version}.jar")
+      expect(java_opts).to include('-DAPPLICATION_INSIGHTS_IKEY=test-instrumentation-key')
+    end
+
+  end
+
+end
