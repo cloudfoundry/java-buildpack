b0388668ee5e251594ba814656aa6ecb5929c0db
Scheidter,Ryan <Ryan.Scheidter@Cerner.com>
2018-11-27 12:36:00 -0600

    Support NewRelic Extensions
    This commit adds support for Custom NewRelic Instrumentation via extension
    files (typically XML/JAR files).  The desired extensions are to be placed in a
    directory and tarred up. The tarfile can then be hosted in a repository in the
    usual way (with `repository_root` and `version` specified in a sub-
    configuration called `extensions`). The buildpack will download this tarfile
    if `extensions.repository_root` is configured, and will unpack the files into
    the 'extensions' directory where the NewRelic agent will look for them by
    default.
    
    [#655]
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- spec/java_buildpack/framework/new_relic_agent_spec.rb
+++ spec/java_buildpack/framework/new_relic_agent_spec.rb
@@ -17,6 +17,7 @@
 
 require 'spec_helper'
 require 'component_helper'
+require 'internet_availability_helper'
 require 'java_buildpack/framework/new_relic_agent'
 require 'java_buildpack/util/tokenized_version'
 
@@ -87,6 +88,68 @@ describe JavaBuildpack::Framework::NewRelicAgent do
       component.release
 
       expect(java_opts).to include('-Dnewrelic.enable.java.8=true')
+    end
+
+    context 'when extensions are configured' do
+
+      extensions_version = '1.2.3'
+      extensions_repo_root = 'test-extensions-repository-root'
+      extensions_uri = 'test-extensions-uri'
+      stubbed_extensions_configuration = { 'repository_root' => extensions_repo_root,
+                                           'version' => extensions_version }
+
+      before do |example|
+        context[:configuration]['extensions'] = example.metadata[:configuration]
+
+        # Extra stubbing for the extensions tarfile fixture
+        extensions_tokenized_version = JavaBuildpack::Util::TokenizedVersion.new(extensions_version)
+        allow(JavaBuildpack::Repository::ConfiguredItem)
+          .to receive(:find_item).with(anything, stubbed_extensions_configuration) do |&block|
+          block&.call(extensions_tokenized_version)
+        end.and_return([extensions_tokenized_version, extensions_uri])
+
+        allow(application_cache).to receive(:get)
+          .with(extensions_uri)
+          .and_yield(Pathname.new('spec/fixtures/stub-new-relic-extensions.tar').open, false)
+      end
+
+      it 'downloads extensions TAR',
+         cache_fixture: 'stub-new-relic-agent.jar',
+         configuration: stubbed_extensions_configuration do
+
+        component.compile
+
+        expect(stdout.string)
+          .to match(/Downloading New Relic Agent Extensions #{extensions_version} from #{extensions_uri}/)
+        expect(sandbox + 'extensions/extension-example.xml').to exist
+      end
+
+      it 'does guarantee that internet access is available when downloading',
+         cache_fixture: 'stub-new-relic-agent.jar',
+         configuration: stubbed_extensions_configuration do
+
+        expect_any_instance_of(JavaBuildpack::Util::Cache::InternetAvailability)
+          .to receive(:available).with(true, 'The New Relic Agent Extensions download location is always accessible')
+
+        component.compile
+      end
+
+      {
+        'missing extensions configuration' => nil,
+        'missing extensions repository' => { 'version' => extensions_version },
+        'blank extensions repository' => { 'repository_root' => '', 'version' => extensions_version }
+      }.each do |non_provided_repository_desc, config|
+        it "ignores #{non_provided_repository_desc}",
+           cache_fixture: 'stub-new-relic-agent.jar',
+           configuration: config do
+
+          component.compile
+
+          expect(stdout.string).not_to match(/New Relic Agent Extensions/)
+          expect(sandbox + 'extensions').not_to exist
+        end
+      end
+
     end
 
   end
