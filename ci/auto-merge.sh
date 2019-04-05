4d165a3977dcad15359b7ac8e391d87bf5b97d12
Ben Hale <bhale@pivotal.io>
2019-04-05 14:31:25 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/auto-merge.sh
+++ ci/auto-merge.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e -u
+set -euo pipefail
 
 pushd upstream
   COMMIT=$(git rev-parse HEAD)
