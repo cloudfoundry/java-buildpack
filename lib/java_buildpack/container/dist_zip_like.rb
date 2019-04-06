3978a5d0194df3f05fb4a3767aaeafc920b9142a
Ben Hale <bhale@pivotal.io>
2019-04-06 07:19:49 -0700

    Revert "Rubocop required changes"
    This reverts commit 8ef27921df6f15648d10428d85925cfd3cffe23b.

--- lib/java_buildpack/container/dist_zip_like.rb
+++ lib/java_buildpack/container/dist_zip_like.rb
@@ -103,9 +103,9 @@ module JavaBuildpack
       def augment_classpath_content
         content = start_script(root).read
 
-        if content.match? PATTERN_CLASSPATH
+        if content =~ PATTERN_CLASSPATH
           augment_classpath content
-        elsif content.match? PATTERN_APP_CLASSPATH
+        elsif content =~ PATTERN_APP_CLASSPATH
           augment_app_classpath content
         end
       end
