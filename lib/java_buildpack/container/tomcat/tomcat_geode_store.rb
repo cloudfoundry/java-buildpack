1a0299e1857aa941cd6d384841db66373414b270
Sarah <sabbey@pivotal.io>
2021-01-29 17:51:20 -0500

    Add security credentils for geode store to gemfire.properties file

--- lib/java_buildpack/container/tomcat/tomcat_geode_store.rb
+++ lib/java_buildpack/container/tomcat/tomcat_geode_store.rb
@@ -44,6 +44,9 @@ module JavaBuildpack
         credentials = @application.services.find_service(FILTER, KEY_LOCATORS, KEY_USERS)['credentials']
         user = credentials[KEY_USERS].find { |u| cluster_operator?(u) }
 
+        File.open(@droplet.root + "gemfire.properties", "w") {|f| f.write("security-username=" + user['username'] +
+                                                                            "\nsecurity-password=" + user['password']) }
+
         @droplet.java_opts.add_system_property 'gemfire.security-username', user['username']
         @droplet.java_opts.add_system_property 'gemfire.security-password', user['password']
         @droplet.java_opts.add_system_property 'gemfire.security-client-auth-init',
