cf9f88762fd3ff85bfd3e0b13ee38ee3e8cbe0f7
Ben Ross <rossb@vmware.com>
2021-02-11 10:39:20 -0800

    Removed unnecessary gemfire.properties

--- lib/java_buildpack/container/tomcat/tomcat_geode_store.rb
+++ lib/java_buildpack/container/tomcat/tomcat_geode_store.rb
@@ -41,13 +41,6 @@ module JavaBuildpack
       def release
         return unless supports?
 
-        credentials = @application.services.find_service(FILTER, KEY_LOCATORS, KEY_USERS)['credentials']
-        user = credentials[KEY_USERS].find { |u| cluster_operator?(u) }
-
-        File.open(@droplet.root + 'gemfire.properties', 'w') do |f|
-          f.write('security-username=' + user['username'] + "\nsecurity-password=" + user['password'])
-        end
-
         @droplet.java_opts.add_system_property 'gemfire.security-client-auth-init',
                                                'io.pivotal.cloudcache.ClientAuthInitialize.create'
       end
