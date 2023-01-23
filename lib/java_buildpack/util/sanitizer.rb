f73c65b428425ead78df5a9847453bb13cbfd681
Rene Bamberger <rene.bamberger@dynatrace.com>
2023-01-23 08:28:41 +0100

    Tests added

--- lib/java_buildpack/util/sanitizer.rb
+++ lib/java_buildpack/util/sanitizer.rb
@@ -18,36 +18,55 @@
 # A mixin that adds the ability to turn a +String+ into sanitized uri
 class String
 
+  # Takes the uri query params and strips out credentials
+  #
+  # @return [String] the sanitized query params
+  def handle_params(params)
+    keywords = /key
+               |password
+               |username
+               |cred(ential)*(s)*
+               |password
+               |token
+               |api[-_]token
+               |api
+               |auth(entication)*
+               |access[-_]token
+               |secret[-_]token/ix
+
+    query_params = ''
+
+    params.each do |key, value|
+      match = key.match(keywords)
+
+      if match
+        params[key] = if match[0] == 'Api-Token' && value =~ /dt\w*/
+                        value.gsub(/(dt\w*\.\w*)\.\w*/, '\1.REDACTED')
+                      else
+                        '***'
+                      end
+      end
+
+      query_params += key + '=' + params[key] + '&'
+    end
+
+    query_params
+  end
+
   # Takes a uri and strips out any credentials it may contain.
   #
   # @return [String] the sanitized uri
   def sanitize_uri
-    keywords = /key|password|username|cred[entials]*[s]*|password|token|api[-_]token|api|auth[entication]*|access[-_]token|secret[-_]token/i
-
     rich_uri          = URI(self)
     rich_uri.user     = nil
     rich_uri.password = nil
 
-    if(rich_uri.query)
-      params = Hash[URI.decode_www_form rich_uri.query]
-
-      query_params = ""
-
-      params.each do |key,value|
-        match = key.match(keywords)
-
-        if(match)
-          if(match[0] == "Api-Token" && value =~ /dt\w*/)
-            params[key] = value.gsub(/(dt\w*\.\w*)\.\w*/, '\1.REDACTED')
-          else
-            params[key] = "***"
-          end
-        end
-
-        query_params += key + "=" + params[key] + "&"
-      end
+    if rich_uri.query
+      params = (URI.decode_www_form rich_uri.query).to_h
+      query_params = handle_params(params)
       rich_uri.query = query_params.chop
     end
+
     rich_uri.to_s
   end
 end
