4d81c3cec75f7b41f0292e129ab96e4d2d6d7136
ushaazul <107583423+ushaazul@users.noreply.github.com>
2023-03-27 18:59:10 -0700

    Update zing_jre.rb
    Fix to address : ERR bash: -Djava.io.tmpdir=temp_dir: No such file or directory

--- lib/java_buildpack/jre/zing_jre.rb
+++ lib/java_buildpack/jre/zing_jre.rb
@@ -40,8 +40,8 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-        super
-        @droplet.add_preformatted_options '-XX:+ExitOnOutOfMemoryError'
+        #super
+        #@droplet.add_preformatted_options '-XX:+ExitOnOutOfMemoryError'
       end
     end
   end
