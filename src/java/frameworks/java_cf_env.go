f4b7d9821ec92ed832fe7e774bc40c71c55e6299
Kiril Keranov <k.keranov@sap.com>
2026-03-10 13:14:06 +0200

    Fix classpath handling for dependency frameworks

--- src/java/frameworks/java_cf_env.go
+++ src/java/frameworks/java_cf_env.go
@@ -1,8 +1,8 @@
 package frameworks
 
 import (
-	"github.com/cloudfoundry/java-buildpack/src/java/common"
 	"fmt"
+	"github.com/cloudfoundry/java-buildpack/src/java/common"
 	"os"
 	"path/filepath"
 	"strings"
@@ -81,16 +81,15 @@ func (j *JavaCfEnvFramework) Finalize() error {
 		return nil
 	}
 
-	// Add to classpath via CLASSPATH environment variable
-	classpath := os.Getenv("CLASSPATH")
-	if classpath != "" {
-		classpath += ":"
-	}
-	classpath += matches[0]
+	depsIdx := j.context.Stager.DepsIdx()
+	runtimePath := fmt.Sprintf("$DEPS_DIR/%s/java_cf_env/%s", depsIdx, filepath.Base(matches[0]))
 
-	if err := j.context.Stager.WriteEnvFile("CLASSPATH", classpath); err != nil {
-		return fmt.Errorf("failed to set CLASSPATH for Java CF Env: %w", err)
+	profileScript := fmt.Sprintf("export CLASSPATH=\"%s${CLASSPATH:+:$CLASSPATH}\"\n", runtimePath)
+	if err := j.context.Stager.WriteProfileD("java_cf_env.sh", profileScript); err != nil {
+		return fmt.Errorf("failed to write java_cf_env.sh profile.d script: %w", err)
 	}
+
+	j.context.Log.Debug("Java CF Env JAR will be added to classpath at runtime: %s", runtimePath)
 
 	return nil
 }
