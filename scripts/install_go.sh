6e81262594bd084be4a54e0419bce9526d5ab94a
Kiril Keranov <114745615+kiril-keranov@users.noreply.github.com>
2026-05-28 11:06:36 +0300

    Update install_go.sh

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -5,24 +5,32 @@ set -u
 set -o pipefail
 
 function main() {
-  if [[ "${CF_STACK:-}" != "cflinuxfs3" && "${CF_STACK:-}" != "cflinuxfs4" ]]; then
+  if [[ "${CF_STACK:-}" != "cflinuxfs3" && "${CF_STACK:-}" != "cflinuxfs4" && "${CF_STACK:-}" != "cflinuxfs5" ]]; then
     echo "       **ERROR** Unsupported stack"
     echo "                 See https://docs.cloudfoundry.org/devguide/deploy-apps/stacks.html for more info"
     exit 1
   fi
 
   local version expected_sha dir
-  version="1.24.6"
-  expected_sha="f39b242d56a2a0d1e0ca5f6294e1811dc9653354603078b128b7f347a33bca2c"
+  version="1.24.13"
+  expected_sha_cflinuxfs4="7ed0876f713f6add32f07c1d82e7ea026b07995c084492c36e6fa27b37d58291"
+  expected_sha_cflinuxfs5="f8361b031b0940ade20ecd61c8e9876be6153f86fa675c4a5befaf5ce69e3ee5"
+  if [[ "${CF_STACK}" == "cflinuxfs4" ]]; then
+    expected_sha="${expected_sha_cflinuxfs4}"
+  elif [[ "${CF_STACK}" == "cflinuxfs5" ]]; then
+    expected_sha="${expected_sha_cflinuxfs5}"
+  else
+    echo "       **ERROR** No SHA defined for stack: ${CF_STACK}"
+    exit 1
+  fi
   dir="/tmp/go${version}"
 
   mkdir -p "${dir}"
 
   if [[ ! -f "${dir}/bin/go" ]]; then
     local url
-    # TODO: use exact stack based dep, after go buildpack has cflinuxfs4 support
-    #url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_${CF_STACK}_${expected_sha:0:8}.tgz"
-    url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_cflinuxfs3_${expected_sha:0:8}.tgz"
+    url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_${CF_STACK}_${expected_sha:0:8}.tgz"
+    # url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_cflinuxfs3_${expected_sha:0:8}.tgz"
 
     echo "-----> Download go ${version}"
     curl "${url}" \
