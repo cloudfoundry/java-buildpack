600f7559c5f0f29ac707cc97dae963dc7b0c6cfb
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2026-06-10 06:58:14 +0000

    fix(tests): correct SapMachine log assertion strings
    #1288 BaseJRE refactor renamed the install log from
    "Installing SAP Machine" to "Installing SapMachine"; four
    assertions in tomcat_test.go and java_main_test.go were missed.

--- src/integration/tomcat_test.go
+++ src/integration/tomcat_test.go
@@ -371,7 +371,7 @@ func testTomcat(platform switchblade.Platform, fixtures string) func(*testing.T,
 
 				Expect(err).NotTo(HaveOccurred(), logs.String)
 
-				Expect(logs.String()).To(ContainSubstring("Installing SAP Machine (17."))
+				Expect(logs.String()).To(ContainSubstring("Installing SapMachine (17."))
 				Eventually(deployment).Should(matchers.Serve(ContainSubstring("OK")))
 			})
 
@@ -384,7 +384,7 @@ func testTomcat(platform switchblade.Platform, fixtures string) func(*testing.T,
 
 				Expect(err).NotTo(HaveOccurred(), logs.String)
 
-				Expect(logs.String()).To(ContainSubstring("Installing SAP Machine (21."))
+				Expect(logs.String()).To(ContainSubstring("Installing SapMachine (21."))
 				Eventually(deployment).Should(matchers.Serve(ContainSubstring("OK")))
 			})
 
@@ -397,7 +397,7 @@ func testTomcat(platform switchblade.Platform, fixtures string) func(*testing.T,
 
 				Expect(err).NotTo(HaveOccurred(), logs.String)
 
-				Expect(logs.String()).To(ContainSubstring("Installing SAP Machine (25."))
+				Expect(logs.String()).To(ContainSubstring("Installing SapMachine (25."))
 				Eventually(deployment).Should(matchers.Serve(ContainSubstring("OK")))
 			})
 		})
